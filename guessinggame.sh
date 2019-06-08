37766dc6108a3eb7da102d51d8c8948d0d448cfe
dgoyzueta <dgoyzueta@gmail.com>
2019-06-08 14:53:30 -0400

    Commit bash file

---
+++ guessinggame.sh
@@ -0,0 +1,25 @@
+clear
+echo "Guess the number of files (including folders) in the directory:"
+pwd
+read number
+
+flag=0
+realnumber=$(ls -l | wc -l)
+
+while [[ flag -eq 0 ]]; do
+    if [[ $number -lt $realnumber ]]; then
+        echo "Too Low."
+    else
+        if [[ $number -gt $realnumber ]]; then
+            echo "Too high."
+        else
+            echo "Congratulations! You got the number right."
+            flag=1
+        fi
+    fi
+    if [[ $flag -eq 0 ]]; then
+        echo "Reenter number:"
+        read number
+    fi
+done
+
